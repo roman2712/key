129b47bc3f354d1850a3bc5829a094060300557d
roman2712 <roman2712@mail.ru>
2018-04-22 00:24:57 +0300

    v2

--- key.h
+++ key.h
@@ -4,85 +4,121 @@
 #define POSITIVE 1
 #define NEGATIVE 0
 
+struct flags_struct{
+	unsigned reading : 1;
+    unsigned buttonStatePush : 1;
+    unsigned lastButtonStatePush : 1;
+	unsigned buttonStatePop : 1;
+	unsigned lastButtonStatePop : 1;
+};
+
 class key
 {
   private:
-    byte k;
-    byte p;
-	
-	byte reading;
-		
-    byte buttonStatePush;
-    byte lastButtonStatePush;
-
-	
-	byte buttonStatePop;
-	byte lastButtonStatePop;
+	flags_struct flags;
 	
     byte debounceDelay;
     unsigned long lastDebounceTime;
-    unsigned long delay;
+	
+	virtual byte readPin();
 
   public:
-	byte readPin();
-    key(byte _k, byte _p = NEGATIVE, byte mode = INPUT_PULLUP, byte _debounceDelay = 50)
+    key(byte _debounceDelay = 50)
     {
-      k = _k;
-      p = _p;
       debounceDelay = _debounceDelay;
-      pinMode(k, mode);
-	  reset();
     }
-    byte read(unsigned long = 0);
-    byte readPop();
+	
+	byte readPush(unsigned long = 0);
+    byte readPop(unsigned long = 0);
 	void reset();
 };
 
 void key::reset()
 {
-	buttonStatePush = lastButtonStatePush = buttonStatePop = lastButtonStatePop = readPin();
+	flags.buttonStatePush = flags.lastButtonStatePush = flags.buttonStatePop = flags.lastButtonStatePop = readPin();
 }
 
-byte key::readPin()
+byte key::readPush(unsigned long _delay)
+{
+  flags.lastButtonStatePush = flags.reading;
+  flags.reading = readPin();
+
+  if (flags.reading != flags.lastButtonStatePush) lastDebounceTime = millis();
+
+  if ((millis() - lastDebounceTime) >  debounceDelay + (flags.reading ^ HIGH ? 0 : _delay)) {
+    if (flags.reading != flags.buttonStatePush) {
+      flags.buttonStatePush = flags.reading;
+      if (flags.buttonStatePush == HIGH) return 1;
+    }
+  }
+  return 0;
+}
+
+byte key::readPop(unsigned long _delay)
+{
+  flags.lastButtonStatePop = flags.reading;
+  flags.reading = readPin();
+
+  if (flags.reading != flags.lastButtonStatePop) lastDebounceTime = millis();
+
+  if ((millis() - lastDebounceTime) >  debounceDelay + (flags.reading ^ LOW ? 0 : _delay)) {
+    if (flags.reading != flags.buttonStatePop) {
+      flags.buttonStatePop = flags.reading;
+      if (flags.buttonStatePop == LOW) return 1;
+    }
+  }
+  return 0;
+}
+
+class digitalKey : public key
+{
+	private:
+	byte k;
+    byte p;
+	
+	byte readPin();
+	
+	public:
+	digitalKey(byte _k, byte _p = NEGATIVE, byte mode = INPUT_PULLUP, byte _debounceDelay = 50) : key(_debounceDelay)
+	{
+	  k = _k;
+      p = _p;
+	  pinMode(k, mode);
+	  reset();
+	}
+};
+
+byte digitalKey::readPin()
 {
   if (p) return digitalRead(k);
   else return !digitalRead(k);
 }
 
-byte key::read(unsigned long _delay)
+class analogKey : public key
 {
-  lastButtonStatePush = reading;
-  reading = readPin();
+	private:
+	byte k;
+    int value_min;
+	int value_max;
+	
+	byte readPin();
+	
+	public:
+	analogKey(byte _k, int _value, byte _range = 50, byte _debounceDelay = 50): key(_debounceDelay)
+	{
+	  k = _k;
+	  value_min = _value-_range;
+	  value_max = _value+_range;
+	  pinMode(k, INPUT);
+	  reset();
+	}
+};
 
-  if (reading != lastButtonStatePush) {
-    lastDebounceTime = millis();
-    if (reading == HIGH) delay = debounceDelay + _delay;
-    else delay = debounceDelay;
-  }
-
-  if ((millis() - lastDebounceTime) >  delay) {
-    if (reading != buttonStatePush) {
-      buttonStatePush = reading;
-      if (buttonStatePush == HIGH) return 1;
-    }
-  }
-  return 0;
-}
-
-byte key::readPop()
+byte analogKey::readPin()
 {
-  lastButtonStatePop = reading;
-  reading = readPin();
-
-  if (reading != lastButtonStatePop) lastDebounceTime = millis();
-
-  if ((millis() - lastDebounceTime) >  delay) {
-    if (reading != buttonStatePop) {
-      buttonStatePop= reading;
-      if (buttonStatePop == LOW) return 1;
-    }
-  }
-  return 0;
+	int temp = analogRead(k);
+	if (value_min < temp && temp < value_max) return true;
+	else return false;
 }
 
 #endif
